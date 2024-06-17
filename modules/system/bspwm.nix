1fa5c53db29d84c1128377c6f9beada5451364b0
meir <michaelmeir1222001@email.com>
2024-06-17 19:46:22 +0200

    fixed bspwm config

--- modules/system/bspwm.nix
+++ modules/system/bspwm.nix
@@ -8,49 +8,51 @@
 with lib;
 let
   buildBspwm = pkgs.writeScript "bspwmrc" (
-    concatStringsSep "\n\n" ["#!/bin/sh"] ++ config.bswpm.rules
+    concatStringsSep "\n\n" [ "#!/bin/sh" ] ++ config.bswpm.rules
   );
 in
 {
   options.bspwm.rules = mkOption {
     type = types.listOf types.str;
-    default = [];
+    default = [ ];
   };
 
-  services.xserver.windowManager.bspwm = {
-    enable = true;
-    configFile = buildBspwm;
-  };
+  config = {
+    services.xserver.windowManager.bspwm = {
+      enable = true;
+      configFile = buildBspwm;
+    };
 
-  sxhkd.keybind = {
-    # close, kill app
-    "super + {_,shift + }q" = ''
-      bspc node -{c,k}
-    '';
+    sxhkd.keybind = {
+      # close, kill app
+      "super + {_,shift + }q" = ''
+        bspc node -{c,k}
+      '';
 
-    # switch between modes
-    "super + m" = ''
-      bspc desktop -l next
-    '';
+      # switch between modes
+      "super + m" = ''
+        bspc desktop -l next
+      '';
 
-    # change mode
-    "super + {t,shift + t,s,f}" = ''
-      bspc node -t {tiled,pseudo_tiled,floating,fullscreen}
-    '';
+      # change mode
+      "super + {t,shift + t,s,f}" = ''
+        bspc node -t {tiled,pseudo_tiled,floating,fullscreen}
+      '';
 
-    # set flag
-    "super + shift + {m,x,y,z}" = ''
-      bspc node -g {marked,locked,sticky,private}
-    '';
+      # set flag
+      "super + shift + {m,x,y,z}" = ''
+        bspc node -g {marked,locked,sticky,private}
+      '';
 
-    # focus/move the node in given direction
-    "super + {_,shift + }{h,j,k,l}" = ''
-      bspc node -{f,s} {west,south,north,east}
-    '';
+      # focus/move the node in given direction
+      "super + {_,shift + }{h,j,k,l}" = ''
+        bspc node -{f,s} {west,south,north,east}
+      '';
 
-    # move (node) to desktop
-    "super + {_,shift + }{1-9,0}" = ''
-      bspc {desktop -f,node -d} '^{1-9,10}'
-    '';
+      # move (node) to desktop
+      "super + {_,shift + }{1-9,0}" = ''
+        bspc {desktop -f,node -d} '^{1-9,10}'
+      '';
+    };
   };
 }
