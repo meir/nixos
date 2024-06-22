b02c06c69b80f374d90a82518c974dd90269b415
meir <michaelmeir1222001@email.com>
2024-06-22 09:29:07 +0200

    regenerated hardware config

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -3,6 +3,32 @@
   config = {
     user = "human";
 
+    # additional hardware configuration
+    boot.initrd.availableKernelModules = [
+      "usb_storage"
+      "sd_mod"
+    ];
+    boot.supportedFilesystems = [ "ntfs" ];
+
+    powerManagement.enable = false;
+    hardware.bluetooth.enable = true;
+
+    hardware.opengl = {
+      enable = true;
+      driSupport = true;
+    };
+
+    hardware.nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      modesetting.enable = true;
+      powerManagement = {
+        enable = true;
+        finegrained = false;
+      };
+      open = false;
+      nvidiaSettings = true;
+    };
+
     bspwm.rules = [
       "bspc wm -O HDMI-1 DP-2"
       "bspc monitor HDMI-1 -d 1 2 3 4 5"
