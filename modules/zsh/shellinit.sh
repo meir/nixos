fd01b5c7d4ec228fce4783d4b8901d437b5ef560
meir <michaelmeir1222001@email.com>
2024-07-16 21:44:14 +0200

    fixed shell

--- modules/zsh/shellinit.sh
+++ modules/zsh/shellinit.sh
@@ -8,13 +8,4 @@ spaceship_prechar() {
   spaceship::section "" "" " " ""
 }
 
-SPACESHIP_PROMPT_ORDER=(prechar dir char)
-SPACESHIP_PROMPT_ADD_NEWLINE=false
-SPACESHIP_PROMPT_SEPARATE_LINE=false
-SPACESHIP_PROMPT_FIRST_PREFIX_SHOW=false
-
-SPACESHIP_DIR_PREFIX=''
-SPACESHIP_CHAR_SYMBOL='‣ '
-SPACESHIP_USER_SHOW=false
-
 [ -d ".git" ] && onefetch || fastfetch
