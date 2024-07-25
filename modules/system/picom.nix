e4f885a2d173aa139a48f9e412ae894d13224417
meir <michaelmeir1222001@email.com>
2024-07-25 12:41:37 +0200

    updated picom config

--- modules/system/picom.nix
+++ modules/system/picom.nix
@@ -14,6 +14,45 @@ with lib;
     vSync = true;
 
     settings = {
+      # Animations
+      animations = true;
+      animation-stiffness-in-tag = 125;
+      animation-stiffness-tag-change = 90.0;
+
+      animation-window-mass = 0.4;
+      animation-dampening = 15;
+      animation-clamping = true;
+
+      animation-for-open-window = "zoom";
+      animation-for-unmap-window = "squeeze";
+      animation-for-transient-window = "slide-up";
+      animation-for-prev-tag = "minimize";
+      enable-fading-prev-tag = "true";
+      animation-for-next-tag = "slide-in-center";
+      enable-fading-next-tag = true;
+
+      # Shadows
+      shadow = false;
+      shadow-radius = 5;
+      shadow-opacity = 0.75;
+      shadow-offset-x = -15;
+      shadow-offset-y = -20;
+
+      # Fading
+      fading = true;
+      fade-in-step = 3.0e-2;
+      fade-out-set = 3.0e-2;
+      fade-delta = 10;
+
+      # Opacity
+      active-opacity = 1.0;
+      inactive-opacity = 0.9;
+      frame-opacity = 1.0;
+
+      # Corners
+      corner-radius = 10;
+
+      # Blurring
       backend = "glx";
 
       blur = {
