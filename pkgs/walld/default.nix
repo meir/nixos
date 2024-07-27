79621c4042bf4524ed1c519c95d62141ac716aad
meir <michaelmeir1222001@email.com>
2024-07-27 23:39:50 +0200

    fixed wall-d

--- pkgs/walld/default.nix
+++ pkgs/walld/default.nix
@@ -1,9 +1,10 @@
 {
+  stdenv,
   lib,
   fetchFromGitHub,
   ...
 }:
-picom.overrideAttrs (previousAttrs: {
+stdenv.mkDerivation {
   pname = "walld";
   version = "02db1883195dbd27509b9a87e20e898201ba0140";
 
@@ -11,11 +12,16 @@ picom.overrideAttrs (previousAttrs: {
     owner = "ronniedroid";
     repo = "Wall-d";
     rev = "02db1883195dbd27509b9a87e20e898201ba0140";
-    hash = "sha256-bXeoWg1ZukXv+6ZNeRc8gGNsbtBztyW5lpfK0lQK+DE=";
+    hash = "sha256-CCLX9pbZE3K4MZbWfpX1STc09Zb1AbNvmPK1Djs4sak=";
   };
 
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ./wall-d $out/bin
+  '';
+
   meta = {
-    mainProgram = "wall-d"
+    mainProgram = "wall-d";
     description = "A simple and fast wallpaper manager";
     homepage = "https://github.com/ronniedroid/Wall-d";
   };
