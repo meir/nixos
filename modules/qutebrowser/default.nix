9396a29470191260cb264dc559dd737ef932b909
meir <michaelmeir1222001@email.com>
2024-07-31 20:26:17 +0200

    fix userscripts

--- modules/qutebrowser/default.nix
+++ modules/qutebrowser/default.nix
@@ -7,18 +7,25 @@
 mkModule config "qutebrowser" {
   environment.packages = with pkgs; [ qutebrowser ];
 
-  environment.file.qutebrowser = {
-    source = ./config.py;
-    target = ".config/qutebrowser/config.py";
-  };
+  environment.file = {
+    qutebrowser = {
+      source = ./config.py;
+      target = ".config/qutebrowser/config.py";
+    };
 
-  environment.file.qutebrowser_homepage = {
-    source = ./homepage;
-    target = ".config/qutebrowser/homepage";
-  };
+    qutebrowser_homepage = {
+      source = ./homepage;
+      target = ".config/qutebrowser/homepage";
+    };
 
-  environment.file.qutebrowser_greasemonkey = {
-    source = ./greasemonkey;
-    target = ".config/qutebrowser/greasemonkey";
+    qutebrowser_greasemonkey = {
+      source = ./greasemonkey;
+      target = ".config/qutebrowser/greasemonkey";
+    };
+
+    qutebrowser_userscripts = {
+      source = ./userscripts;
+      target = ".local/share/qutebrowser/userscripts";
+    };
   };
 }
