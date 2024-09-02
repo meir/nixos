4c7a1319131b6c68410178cea80d339ce914786a
meir <contact@flamingo.dev>
2024-09-02 13:21:28 +0200

    wip

--- hosts/work-macos/configuration.nix
+++ hosts/work-macos/configuration.nix
@@ -1,5 +1,8 @@
 { lib, pkgs, ... }:
 {
+  user = "meir";
+  user_home = "/Users/meir";
+
   home-manager.users.meir = {
     home = {
       username = "meir";
--- system/darwin/user.nix
+++ system/darwin/user.nix
@@ -1,7 +1,42 @@
-{ config, pkgs, ... }:
 {
-  users.users.${config.my.user} = {
-    home = "/Users/${config.my.user}";
-    shell = pkgs.zsh;
+  options,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+{
+  options = {
+    user = mkOption {
+      type = types.str;
+      default = "human";
+      description = "Name of the default user";
+    };
+    user_home = mkOption {
+      type = types.path;
+      default = "/home/${config.user}";
+      internal = true;
+      description = "Home directory of the default user";
+    };
+  };
+
+  config = {
+    environment.variables = {
+      "XDG_CONFIG_HOME" = "${config.user_home}/.config";
+      "XDG_DATA_HOME" = "${config.user_home}/.local/share";
+    };
+
+    users.users."${config.user}" = {
+      isNormalUser = true;
+      home = config.user_home;
+      initialPassword = "nixos";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "desktop"
+        "docker"
+      ];
+    };
   };
 }
