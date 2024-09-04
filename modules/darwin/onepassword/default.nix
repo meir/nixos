38de7a7202c42313ad0585a53284501019cbc015
meir <meir@MacBook-Pro.local>
2024-09-04 15:40:50 +0200

    fixed onepassword on darwin

--- modules/darwin/onepassword/default.nix
+++ modules/darwin/onepassword/default.nix
@@ -9,9 +9,9 @@ with lib;
   options.modules.onepassword.enable = mkEnableOption "onepassword";
 
   config = mkIf config.modules.onepassword.enable {
-    environment.systemPackages = with pkgs; [
-      _1password-gui
-      _1password
-    ];
+    homebrew = {
+      enable = true;
+      casks = [ "1password" ];
+    };
   };
 }
