53196d462f74aed5d18c739dd32d46769acda029
meir <michaelmeir1222001@email.com>
2024-09-19 18:00:08 +0200

    add osx-kvm options

--- modules/linux/docker/default.nix
+++ modules/linux/docker/default.nix
@@ -11,6 +11,15 @@ with lib;
   config = mkIf config.modules.docker.enable {
     virtualisation.docker.enable = true;
 
+    virtualisation.libvirtd.enable = true;
+    users.extraUsers.youruser.extraGroups = [ "libvirtd" ];
+
+    boot.extraModprobeConfig = ''
+      options kvm_intel nested=1
+      options kvm_intel emulate_invalid_guest_state=0
+      options kvm ignore_msrs=1
+    '';
+
     environment.systemPackages = with pkgs; [ docker ];
   };
 }
