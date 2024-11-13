92fa23878cbdadaa8e366a58c305710d0f5ed88a
meir <michaelmeir1222001@email.com>
2024-11-13 23:21:52 +0100

    wip

--- modules/virtualization/default.nix
+++ modules/virtualization/default.nix
@@ -22,63 +22,63 @@ with lib;
   };
 
   config = mkIf config.modules.vfio.enable {
-    virtualisation.libvirtd = {
-      enable = true;
-      onBoot = "ignore";
-      onShutdown = "shutdown";
-      qemu = {
-        ovmf.enable = true;
-        runAsRoot = true;
-      };
-    };
-
-    programs.dconf.profiles.user.databases = [
-      {
-        lockAll = true;
-
-        settings = {
-          "org/virt-manager/virt-manager/connections" = {
-            autoconnect = [ "qemu:///system" ];
-            uris = [ "qemu:///system" ];
-          };
-        };
-      }
-    ];
-
-    programs.virt-manager.enable = true;
-    users.extraUsers."${config.user}".extraGroups = [ "libvirtd" ];
-
-    boot =
-      let
-        ids = lib.concatStringsSep "," config.modules.vfio.pci_ids;
-      in
-      {
-        extraModprobeConfig = ''
-          options vfio-pci ids=${ids}
-          options kvm_amd nested=1
-          options kvm_amd emulate_invalid_guest_state=0
-          options kvm ignore_msrs=1
-        '';
-
-        kernelParams = [
-          "amd_iommu=on"
-          "iommu=pt"
-          "vfio-pci.ids=${ids}"
-        ];
-        kernelModules = [
-          "vfio"
-          "vfio_iommu_type1"
-          "vfio_pci"
-          "vfio_virqfd"
-          "kvm"
-          "kvm_amd"
-        ];
-      };
+    # virtualisation.libvirtd = {
+    #   enable = true;
+    #   onBoot = "ignore";
+    #   onShutdown = "shutdown";
+    #   qemu = {
+    #     ovmf.enable = true;
+    #     runAsRoot = true;
+    #   };
+    # };
+    #
+    # programs.dconf.profiles.user.databases = [
+    #   {
+    #     lockAll = true;
+    #
+    #     settings = {
+    #       "org/virt-manager/virt-manager/connections" = {
+    #         autoconnect = [ "qemu:///system" ];
+    #         uris = [ "qemu:///system" ];
+    #       };
+    #     };
+    #   }
+    # ];
+    #
+    # programs.virt-manager.enable = true;
+    # users.extraUsers."${config.user}".extraGroups = [ "libvirtd" ];
+    #
+    # boot =
+    #   let
+    #     ids = lib.concatStringsSep "," config.modules.vfio.pci_ids;
+    #   in
+    #   {
+    #     extraModprobeConfig = ''
+    #       options vfio-pci ids=${ids}
+    #       options kvm_amd nested=1
+    #       options kvm_amd emulate_invalid_guest_state=0
+    #       options kvm ignore_msrs=1
+    #     '';
+    #
+    #     kernelParams = [
+    #       "amd_iommu=on"
+    #       "iommu=pt"
+    #       "vfio-pci.ids=${ids}"
+    #     ];
+    #     kernelModules = [
+    #       "vfio"
+    #       "vfio_iommu_type1"
+    #       "vfio_pci"
+    #       "vfio_virqfd"
+    #       "kvm"
+    #       "kvm_amd"
+    #     ];
+    #   };
 
     environment.systemPackages = with pkgs; [
       pciutils
-      qemu
       virt-manager
+      quickemu
     ];
   };
 }
