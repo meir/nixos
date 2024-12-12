a440b8ecaf9ccf8d8d51ed8c92ff5902e82e02eb
meir <me@flamingo.dev>
2024-12-12 10:27:30 +0100

    fix replace

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -1,10 +1,10 @@
 { pkgs, izu, ... }:
 with pkgs;
 final: prev: {
-  cozette-nerdfont = callPackage ../pkgs/cozette-nerdfont final;
-  dina-remastered = callPackage ../pkgs/dina-remastered final;
-  cdl = callPackage ../pkgs/cdl final;
-  walld = callPackage ../pkgs/walld final;
-  replace = callPackage ../pkgs/replace final;
-  izu = callPackage ../pkgs/izu final;
+  cozette-nerdfont = callPackage ../pkgs/cozette-nerdfont { };
+  dina-remastered = callPackage ../pkgs/dina-remastered { };
+  cdl = callPackage ../pkgs/cdl { };
+  walld = callPackage ../pkgs/walld { };
+  replace = callPackage ../pkgs/replace { };
+  izu = callPackage ../pkgs/izu { };
 }
