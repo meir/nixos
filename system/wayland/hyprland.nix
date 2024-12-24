1c92088e7d2371851804e1b56e479d0e4e1fb5e6
meir <me@flamingo.dev>
2024-12-24 14:43:46 +0100

    wip to wayland

--- system/wayland/hyprland.nix
+++ system/wayland/hyprland.nix
@@ -6,9 +6,7 @@
 }:
 with lib;
 let
-  startup = concatStringsSep "\n\n" (
-    concatMap (value: "exec-once = ${value}") config.protocol.autostart
-  );
+  startup = concatMap (value: "exec-once = ${value}") config.protocol.autostart;
   binds = pkgs.izuGenerate "hyprland" config.protocol.hotkeys;
 
   # bind = Super, Return, exec, kitty
