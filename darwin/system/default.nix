922a2d28bcff0c72a0c745b19279e3c239afc87a
meir <contact@flamingo.dev>
2025-04-16 16:40:44 +0200

    fix flake

--- darwin/system/default.nix
+++ darwin/system/default.nix
@@ -19,7 +19,6 @@
     curl
   ];
 
-  services.nix-daemon.enable = true;
   nixpkgs.config.allowUnfree = true;
 
   system.stateVersion = 4;
--- flake.nix
+++ flake.nix
@@ -1,13 +1,10 @@
-let
-  release = "24.11";
-in
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-${release}";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-${release}";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    stylix.url = "github:danth/stylix/release-${release}";
+    stylix.url = "github:danth/stylix/release-24.11";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
     stylix.inputs.home-manager.follows = "home-manager";
 
