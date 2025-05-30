6888aa9c2ac77c6efd9bcde3712621b46e830c48
meir <contact@flamingo.dev>
2025-05-30 10:16:51 +0200

    wip

--- pkgs/seanime/default.nix
+++ pkgs/seanime/default.nix
@@ -5,7 +5,7 @@ pkgs.stdenv.mkDerivation rec {
   version = "2.8.4";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/5rahim/seanime/releases/download/v${version}/seanime-${version}_Linux_x86_64.tar.gz";
+    url = "https://github.com/5rahim/seanime/releases/download/v${version}/seanime-desktop-${version}_Linux_x86_64.AppImage";
     hash = "sha256-Yjt+Oq42QMd4H6MtzZlSaDYav5EEOakYkWTCxzyc8M4=";
   };
 
@@ -13,8 +13,21 @@ pkgs.stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out/bin
-    tar xzf $src -C $out/bin
+    mv $src $out/bin
   '';
+
+  desktopItems = [
+    {
+      name = "SeAnime";
+      exec = "${pname}";
+      icon = "seanime";
+      type = "Application";
+      categories = [
+        "AudioVideo"
+        "Network"
+      ];
+    }
+  ];
 
   meta = {
     description = "Open-source media server with a web interface and desktop app for anime and manga";
