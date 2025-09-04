c15303a2041243004e89ce74c5d2559b4c6bfacb
meir <michaelmeir1222001@email.com>
2025-09-04 16:40:05 +0200

    go back on stable

--- hosts/desktop/system.nix
+++ hosts/desktop/system.nix
@@ -1,12 +1,12 @@
 {
-  nixpkgs-unstable,
+  nixpkgs,
   home-manager,
   base16,
   stylix,
   ...
 }:
 specialArgs:
-nixpkgs-unstable.lib.nixosSystem {
+nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
   inherit specialArgs;
 
