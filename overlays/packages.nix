bca883ea95203daeedc221ce5bead72de4fe541f
meir <michaelmeir1222001@email.com>
2025-11-04 10:39:34 +0100

    use index controller supported fork

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -7,4 +7,13 @@ final: prev: {
   discord_wayland = import ../pkgs/discord_wayland final;
   sm64coopdx-local = callPackage ../pkgs/sm64coopdx { };
   zen-browser = zen-browser.packages."${final.system}";
+  monado = monado.overrideAttrs (old: {
+    src = fetchFromGitLab {
+      domain = "gitlab.freedesktop.org";
+      owner = "Supreeeme";
+      repo = "monado";
+      rev = "e57d5411f53ab72e715b4ce91d26ff96c80ac711";
+      hash = "sha256-VxTxvw+ftqlh3qF5qWxpK1OJsRowkRXu0xEH2bDckUA=";
+    };
+  });
 }
