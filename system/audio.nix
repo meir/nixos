62844bb9fc38d83d565b3f6fb3cd69630172da49
meir <michaelmeir1222001@email.com>
2025-11-08 22:09:05 +0100

    try to fix audio

--- system/audio.nix
+++ system/audio.nix
@@ -29,8 +29,8 @@ with lib;
     wireplumber.enable = true;
     extraConfig.pipewire = {
       context.properties = {
-        default.clock.rate = 192000;
-        defautlt.allowed-rates = [ 192000 ];
+        default.clock.rate = 48000;
+        defautlt.allowed-rates = [ 48000 ];
         default.clock.quantum = 32;
         default.clock.min-quantum = 32;
         default.clock.max-quantum = 32;
