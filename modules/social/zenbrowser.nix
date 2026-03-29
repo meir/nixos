93a30896d28f59ea21fdd280cb920e3e035cdccc
meir <michaelmeir1222001@email.com>
2026-03-29 12:52:58 +0200

    wip

--- modules/social/zenbrowser.nix
+++ modules/social/zenbrowser.nix
@@ -1,39 +1,8 @@
 { pkgs, lib, ... }:
-let
-  pywalfoxUpdateHandleSocket = pkgs.writeShellScript "pywalfox-update-handle-socket" ''
-    set -euo pipefail
-
-    sock=/tmp/pywalfox_socket
-    if [ ! -S "$sock" ]; then
-      rm -f $sock 2>/dev/null
-    fi
-
-    exec ${lib.getExe pkgs.pywalfox-native} update
-  '';
-in
 {
   environment.systemPackages = with pkgs; [
     zen-browser
-    pywalfox-native
   ];
-
-  systemd.user.services.pywalfox-update = {
-    partOf = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = pywalfoxUpdateHandleSocket;
-      PrivateTmp = false;
-    };
-  };
-
-  systemd.user.paths.pywalfox-update = {
-    wantedBy = [ "graphical-session.target" ];
-    pathConfig = {
-      PathChanged = "%h/.cache/wal/colors.json";
-      Unit = "pywalfox-update.service";
-    };
-  };
 
   xdg.mime.defaultApplications = {
     "text/html" = "zenbrowser.desktop";
